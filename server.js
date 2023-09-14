119fa22c6dada424a4df67c314b50a89b8ffc027
Umar Shaikh <shaikh.umar.iqra@gmail.com>
2023-09-14 11:42:02 +0530

    servercommit

--- server.js
+++ server.js
@@ -1,19 +1,21 @@
 const express = require("express");
 const app = express();
-const server = require("http").Server(app);
+const httpServer = require("http").createServer(app); // Use createServer to initialize the HTTP server
 const { v4: uuidv4 } = require("uuid");
-app.set("view engine", "ejs");
-const io = require("socket.io")(server, {
-  cors: {
-    origin: '*'
-  }
-});
 const { ExpressPeerServer } = require("peer");
 const opinions = {
   debug: true,
-}
+};
 
-app.use("/peerjs", ExpressPeerServer(server, opinions));
+const serverPort = process.env.PORT || 3030;
+
+app.set("view engine", "ejs");
+
+// Initialize ExpressPeerServer with the httpServer
+const peerServer = ExpressPeerServer(httpServer, opinions);
+
+app.use("/peerjs", peerServer);
+
 app.use(express.static("public"));
 
 app.get("/", (req, res) => {
@@ -24,16 +26,24 @@ app.get("/:room", (req, res) => {
   res.render("room", { roomId: req.params.room });
 });
 
+const io = require("socket.io")(httpServer, {
+  cors: {
+    origin: '*'
+  }
+});
+
 io.on("connection", (socket) => {
   socket.on("join-room", (roomId, userId, userName) => {
     socket.join(roomId);
-    setTimeout(()=>{
+    setTimeout(() => {
       socket.to(roomId).broadcast.emit("user-connected", userId);
-    }, 1000)
+    }, 1000);
     socket.on("message", (message) => {
       io.to(roomId).emit("createMessage", message, userName);
     });
   });
 });
 
-server.listen(process.env.PORT || 3030);
+httpServer.listen(serverPort, () => {
+  console.log(`Server is running on port ${serverPort}`);
+});
